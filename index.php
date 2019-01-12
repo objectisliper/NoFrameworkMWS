249e2ee595bb05a0f8d0c554e83753b9e32b60ac
objectisliper <objectisliper@gmail.com>
2019-01-12 14:54:14 +0200

    init

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: object
+ * Date: 12.01.19
+ * Time: 14:32
+ */
---
+++ src/Bootstrap.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: object
+ * Date: 12.01.19
+ * Time: 14:43
+ */
+
+declare(strict_types = 1);
+
+echo 'Hello World!';
